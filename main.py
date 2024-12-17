46d4bfd1c67a5dc446e9c48ffadbc88ce10b6321
thlurte <s92062851@ousl.lk>
2024-12-17 11:33:53 +0530

    ED85#T: count words

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser("Word Counter")
 parser.add_argument('-p','--path',help='Path of Text File',required=True)
 parser.add_argument('-c','--character',help='Count Characters',action='store_true')
+parser.add_argument('-w','--word',help='Count Words',action='store_true')
 
 
 args = vars(parser.parse_args())
@@ -12,3 +13,8 @@ args = vars(parser.parse_args())
 # Step - 01: return the number of byters in the file
 if args['character']:
     print(os.stat(args['path']).st_size)
+
+# Step - 03: return the word count
+elif args['word']:
+    with open(args['path'],'r') as file:
+        print(len(file.read().split(' ')))
